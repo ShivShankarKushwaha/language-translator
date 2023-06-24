03cb689fe1dff00e1375fdc705e58e02161dea13
ShivshankarKushwaha <shivshankarkushwaha0000@gmail.com>
2023-06-24 17:00:09 +0530

    node-fetch

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bp = require('body-parser');
 const port = process.env.PORT || 5500;
+const fetch = require('node-fetch');
 
 app.use(express.static(__dirname+"/public"))
 app.use(bp.urlencoded({extended:true}));
