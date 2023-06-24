a61349d15ae499da4a120e4cfc30ce5c6fb8116a
ShivshankarKushwaha <shivshankarkushwaha0000@gmail.com>
2023-06-24 17:13:53 +0530

    dynamic node-fetch

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const express = require('express');
 const app = express();
 const bp = require('body-parser');
 const port = process.env.PORT || 5500;
-const fetch = require('node-fetch');
 
-app.use(express.static(__dirname+"/public"))
-app.use(bp.urlencoded({extended:true}));
-app.get("/",(req,res)=>
-{
-    res.sendFile(__dirname+"/public/index.html");
+app.use(express.static(__dirname + "/public"));
+app.use(bp.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
 });
-app.post("/",async(req,res)=>{
+
+app.post("/", async (req, res) => {
     console.log(req.body);
     let text = req.body.text;
     let source_lang = req.body.source_lang;
@@ -20,21 +20,25 @@ app.post("/",async(req,res)=>{
         method: 'POST',
         body: new URLSearchParams({ 'text': text, 'target_lang': target_lang, 'source_lang': source_lang })
     };
-    fetch(process.env.URL, option)
-        .then(responce => responce.text())
-        .then(result => {
-            console.log(result);
-            result = JSON.parse(result);
-            if(result.status=='success')
-            {
-                return res.json({'status':'success','target_lang':result.target_lang,'text':result.text});
-            }
-            else
-            {
-                return res.json({status:'error',text:'Error Occured'})
-            }
-        })
-        .catch(err => { res.send('hello'); })
-})
 
-app.listen(port);
+    try {
+        const { default: fetch } = await import('node-fetch');
+        const response = await fetch(process.env.URL, option);
+        const result = await response.text();
+        console.log(result);
+        const parsedResult = JSON.parse(result);
+
+        if (parsedResult.status === 'success') {
+            return res.json({ 'status': 'success', 'target_lang': parsedResult.target_lang, 'text': parsedResult.text });
+        } else {
+            return res.json({ status: 'error', text: 'Error Occurred' });
+        }
+    } catch (err) {
+        console.error(err);
+        res.send('An error occurred');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
